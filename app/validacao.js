15902a33127338bf6bcd2253f3b42e93f2603199
Arruda Junior <juniorarruda15@gmail.com>
2024-06-14 09:12:12 -0300

    Correção na logica de validação doi numero valido

--- app/validacao.js
+++ app/validacao.js
@@ -15,7 +15,6 @@ function verificaSeOChuteValido (chute) {
 
             elementoChute.innerHTML += '<div>Valor Inválido</div>';
         }
-        return
     }
 
     if (numeroForMaiorOuMenorPermitido(numero)) {
@@ -30,14 +29,14 @@ function verificaSeOChuteValido (chute) {
 
             <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
-    } else if (numeroMaior > numeroSecreto) {
+    } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
-            <div> O número secreto é menor <i class="fa-solid fa-arrow-dow"></i></div>
+            <div> O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
     } else {
-        `<div> O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>`
+        `<div> O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
     }
-
+}
 
 function chuteForInvalido(numero) {
     return Number.isNaN(numero);
