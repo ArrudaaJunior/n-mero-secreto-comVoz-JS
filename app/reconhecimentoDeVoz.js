1c36bac5598f7dfb3cbb6ae673eecd400b820386
Arruda Junior <juniorarruda15@gmail.com>
2024-06-14 07:54:58 -0300

    Implementando o web speech API

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,7 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br';
+recognition.start();
